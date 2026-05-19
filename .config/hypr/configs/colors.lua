ae9ec59eb9926983bdae17048454fa455ee1dfb5
hireri <187438434+hireri@users.noreply.github.com>
2026-05-19 20:22:01 +0200

    yo.

---
+++ .config/hypr/configs/colors.lua
@@ -0,0 +1,20 @@
+hl.config({
+    general = {
+        col = {
+            active_border = {
+                colors = {"#f6bc70", "#bacd9f"}
+            },
+            inactive_border = "#4f4539"
+        }
+    },
+    misc = {
+        background_color = "rgba(18120cFF)"
+    }
+})
+
+hl.window_rule({
+    match = {
+        pin = 1
+    },
+    border_color = "#ffb4ab"
+})
